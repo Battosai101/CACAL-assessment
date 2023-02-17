4909ad339e50c2bee281fd9ceb84c6b14d613b3e
Battosai101 <cyrruzzc@gmail.com>
2023-02-17 10:40:57 +0800

    About us back to original

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,7 +2,7 @@ export default function AboutUs(){
     return(
         <div className="aboutus">
             <div className="aboutus--content">
-                This is "About Us Page Hello world"
+                This is "About Us Page"
             </div>
         </div>
     )
