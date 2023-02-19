e728e9aa95ce8d2658499b2d09eb8ca3c941be8c
Battosai101 <cyrruzzc@gmail.com>
2023-02-19 22:33:15 +0800

    test

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,7 +1,7 @@
 export default function Episode(){
     return(
         <div className="relative bg-[#1A1A1A] h-screen">
-            <h1 className='text-white'>This is Episode section</h1>
+            <h1 className='text-white'>This is Episode section hello</h1>
         </div>
     )
 }
