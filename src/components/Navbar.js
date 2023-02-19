d3874fd1bd14c7adda3a6b2266f04bdfce6de20d
Battosai101 <cyrruzzc@gmail.com>
2023-02-19 20:22:06 +0800

    Fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,41 @@
 import {NavLink} from 'react-router-dom'
 export default function Navbar(){
     return(
-        <div className='flex items-center justify-between bg-black w-full pt-24 flex-wrap'>
-            <h1 className='text-white w-[135px] h-[30px] left-[90px] top-[75px] text-2xl font-bold font-[Outfit] cursor-pointer pl-[90px]'>AnimeBinge</h1>
+        <nav className='absolute flex items-center justify-between z-10 w-full px-[90px] text-white flex-wrap'>
+            <h1 className='font-[Outfit] text-2xl font-bold items-center pt-[75px]'>
+                AnimeBinge
+            </h1>
+            <div className='font-[Montserrat] text-white font-normal text-base items-center pt-[81px] '>
+                <NavLink to='/'>
+                    <button className='mr-[53px]'>
+                        Home
+                    </button>
+                </NavLink>
+                <NavLink to='/discover'>
+                    <button className='mr-[58px]'>
+                        Discover
+                    </button>
+                </NavLink>
+                <NavLink to='/aboutus'>
+                    <button className='mr-[49px]'>
+                        About Us
+                    </button>
+                </NavLink>
+                <NavLink to='/signup'>
+                    <button className='mr-[21px] px-7 py-3 border-[1px] border-white rounded-md'>
+                        Sign Up
+                    </button>
+                </NavLink>
+                <NavLink to='/login'>
+                    <button className='px-8 py-3 rounded-md bg-[#106580]'>
+                        Log In
+                    </button>
+                </NavLink>
+            </div>
+        </nav>
+        /*
+        <div className='flex items-center justify-between bg-black w-full pt-24 px-[90px] flex-wrap'>
+            <h1 className='text-white w-[135px] h-[30px] top-[75px] text-2xl font-bold font-[Outfit] cursor-pointer'>AnimeBinge</h1>
             <div className='flex items-center text-white'>
                 <NavLink to='/'><button className='mr-4 hover:underline'>Home</button></NavLink>
                 <NavLink to='/discover'><button className='mr-4 hover:underline'>Discover</button></NavLink>
@@ -10,6 +43,6 @@ export default function Navbar(){
                 <NavLink to='/signup'><button className='mr-4 hover:underline rounded-md border-[1px] border-white px-6 py-2'>Sign Up</button></NavLink>
                 <NavLink to='/login'><button className='bg-[#106580] px-6 py-2 rounded-md cursor-pointer mr-4 hover:underline'>Log In</button></NavLink>
             </div>
-        </div>
+        </div>*/
     )
 }
