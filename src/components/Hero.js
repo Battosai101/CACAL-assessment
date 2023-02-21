d110ddbcb56ae7ca22abe2d853c553af7dfa50be
Battosai101 <cyrruzzc@gmail.com>
2023-02-21 16:57:02 +0800

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ export default function Hero(){
     return(
         <div className='hero top-0 relative bg-center h-[150vh] w-full bg-cover bg-no-repeat'>
             <HeroInfo />
-            <div className='absolute w-full h-[150vh] bg-gradient-to-r via-transparent from-black to-black text-white'>
+            <div className='absolute h-[150vh] w-full bg-gradient-to-r from-black via-transparent to-black'>
             </div>
         </div>
     )
--- src/components/TrendCard.js
+++ src/components/TrendCard.js
@@ -1,10 +1,30 @@
-//import AoT from '../AoT.png'
+import aot from '../aot.png'
+import star from '../star.png'
 export default function TrendCard(){
     return(
-           <button className='w-[516px] h-[291px] shrink-0 rounded-[21px] overflow-hidden snap-start border-2 border-yellow-300'>
-               
+           <button className='relative w-[516px] h-[291px] shrink-0 rounded-[21px] overflow-hidden snap-start hover:shadow-[0_0_0_6px_rgba(196,196,196,0.42)] hover:duration-300 hover:ease-out'>
+                <img src={aot} alt="AoT" className='w-full h-full'/>
+                <div className='absolute top-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
+                <div className='absolute w-full flex flex-col pl-[33px] top-[223px] bottom-5 text-white'>
+                    <h1 className=' flex justify-start items-center font-[Outfit] font-medium text-2xl'>
+                        Attack on Titan
+                    </h1>
+                    <div className='flex items-center'>
+                        <p className='font-[Outfit] font-normal text-xs text-[#747474] mr-[116.51px]'>
+                            Category: Adventure fiction, Dark fantasy, Martial Arts
+                        </p>
+                        <div className='flex items-center mr-5'>
+                            <img src={star} alt="star icon" className='w-[19.97px] h-[18.99px] mr-1' />
+                            <p className='font-[Outfit] font-medium text-xs'>
+                                5.0
+                            </p>
+                        </div>
+                    </div>
+                </div>
            </button>
     )
 }
+/*
+hover:shadow-md hover:shadow-white hover:duration-300 hover:ease-out*/
 
-//<img src={AoT} alt="AoT" className='w-full h-full border-2 border-blue-900'/>
+
