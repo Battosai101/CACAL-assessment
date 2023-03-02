d3de37e753e302cbac6bb2cd0ce4ca9bf0ee1ea1
Battosai101 <cyrruzzc@gmail.com>
2023-03-02 16:41:07 +0800

    add search functionality

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -2,9 +2,15 @@ import TrendCard from './TrendCard'
 import ThrowCard from './ThrowCard'
 import TrendData from '../data/TrendData'
 import ThrowData from '../data/ThrowData'
+import {useState} from 'react'
 
 export default function Episode(){
-    const trend = TrendData.map(item => {
+    
+    const [searchValue, setSearchValue] = useState("")
+    console.log(searchValue.toLowerCase())
+    const trend = TrendData.filter(item => {
+        return searchValue.toLowerCase() === "" ? item : item.title.toLowerCase().includes(searchValue.toLowerCase())
+    }).map(item => {
         return <TrendCard 
                 key={item.id}
                 id={item.id}
@@ -27,15 +33,21 @@ export default function Episode(){
 
     return(
         <div className='relative font-[Outfit] w-full bg-[#1A1A1A]'>
-            <h1 className='text-white font-bold text-[34px] leading-[43px] pt-14 pl-[89px]'>
-                Trending <span className='text-[#FBC94A]'>this week</span>
-            </h1>
+            <div className='relative flex items-center justify-between'>
+                <h1 className='text-white font-bold text-[34px] leading-[43px] mt-14 ml-[89px]'>
+                    Trending <span className='text-[#FBC94A]'>this week</span>
+                </h1>
+                <input type="search" placeholder="Search..." className='self-end mr-[90px] rounded-lg pl-3' onChange={(e) => setSearchValue(e.target.value)} />
+            </div>
             <div className='text-white flex items-center mt-[26px] px-[90px] space-x-4 py-2 overflow-auto scrollbar-hide scroll-smooth scroll-pl-[90px] snap-x'>
                 {trend}
             </div>
-            <h1 className=' text-white font-bold text-[34px] leading-[43px] mt-14 pl-[89px]'>
-                Throwback Anime!
-            </h1>
+            <div className='relative flex items-center justify-between'>
+                <h1 className=' text-white font-bold text-[34px] leading-[43px] mt-14 ml-[89px]'>
+                    Throwback Anime!
+                </h1>
+                <input type="search" placeholder="Search..." className='self-end mr-[90px] rounded-lg pl-3' />
+            </div>
             <div className='mt-9 pb-14 flex items-center px-[90px] space-x-5 overflow-auto scrollbar-hide scroll-smooth scroll-pl-[90px] snap-x'>
                 {throwback}
             </div>
